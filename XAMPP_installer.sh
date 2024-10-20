d6feba774ea2b8ec2bc598fb781790d9c00aa3bc
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-10-20 13:38:25 +0800

    Fixed dvwa_installer.sh

--- XAMPP_installer.sh
+++ XAMPP_installer.sh
@@ -1,6 +1,9 @@
+echo "Installing dependancied"
+sudo dnf -y update
+sudo dnf -y install libnsl
 echo "Installing XAMPP"
 
 chmod +x xampp-installer.run
-sudo ./xampp-installer,run
 
 echo "Complete XAMPP installation through GUI"
+sudo /.xampp-installer.run
