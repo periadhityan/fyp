28bc188141da66a2cd1e756aa74da327ab1c9ff4
periadhityan <44988530+periadhityan@users.noreply.github.com>
2024-10-20 21:01:41 +0800

    Working DVWA installer

--- dvwa_installer.sh
+++ dvwa_installer.sh
@@ -5,12 +5,9 @@ sudo git clone https://github.com/digininja/DVWA
 echo "Setting up configuration files"
 
 sudo cp /opt/lampp/htdocs/DVWA/config/config.inc.php.dist /opt/lampp/htdocs/DVWA/config/config.inc.php
-sudo cp -r /opt/lampp/htdowc/DVWA/* /opt/lampp/htdocs
+sudo cp -r /opt/lampp/htdocs/DVWA/* /opt/lampp/htdocs
 
-sudo sh -c "sed \"s/\[ 'db_user' \] = 'dvwa'/\[ 'db_user' \] =
-'root'/;s/\[ 'db_password' \] = 'p@ssw0rd'/\[ 'db_password' \] = ''/\"
-/opt/lampp/htdocs/config/config.inc.php.dist >
-/opt/lampp/htdocs/config/config.inc.php"
+sudo cp /home/$USER/fyp/dvwaconfig.txt /opt/lampp/htdocs/DVWA/config/config.inc.php
 
-sudo sed -i "s/dashboard/login/php" /opt/lampp/htdocs/index.php
+#sudo sed -i "s/dashboard/login/php" /opt/lampp/htdocs/index.php
 
