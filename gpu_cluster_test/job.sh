cad81d08bf769f3169fc605ccbfda61b39563a7a
periadhityan <periadhityan@gmail.com>
2024-12-27 12:08:56 +0800

    First test job

---
+++ gpu_cluster_test/job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=UGGPU-TC1
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8G
+#SBATCH --time=60
+#SBATCH --job-name=MyJob
+#SBATCH --output=output_%x%j.out
+#SBATCH --error=error_%x_%j.err
+
+
+
+
+
+
+
+
+
+module load cuda/11.8
+module load anaconda
+source activate TestEnv
+python 01_creating_graphs.py
