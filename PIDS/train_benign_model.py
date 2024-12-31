dfe258a77224418eca1b7c90f1545e84780c72e4
periadhityan <periadhityan@gmail.com>
2024-12-31 16:27:15 +0800

    Minor edits

--- PIDS/train_benign_model.py
+++ PIDS/train_benign_model.py
@@ -1,16 +1,10 @@
 import dgl
 import torch
-import json
-import os
-import dgl.function as fn
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import dgl.nn as dglnn
-import torch.nn.functional as F
-from tqdm import tqdm
 from torch.optim import Adam
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+
+from graph_creation import CreatingGraphs
 from model import HeteroClassifier
 
 
@@ -63,47 +57,6 @@ def main():
 
     torch.save(model, "Benign_Model_32_Feat.pth")
 
-
-
-
-def CreatingGraphs(graphs_folder, graphs_type):
-
-    graphs = []
-
-    for file in tqdm(os.listdir(graphs_folder), desc="Creating Graphs", unit="Graphs"):
-        json_file = os.path.join(graphs_folder, file)
-        g = create_graph(json_file)
-        graphs.append(g)
-
-    if graphs_type == "benign":
-        labels = {'labels': torch.zeros(len(graphs))}
-    else:
-        labels = {'labels': torch.ones(len(graphs))}
-
-    return graphs, labels
-
-def create_graph(file):
-
-    with open(file, "r") as f:
-        data = json.load(f)
-
-    graph_data = {}
-
-    for edge, node_pairs in data.items():
-        stype, etype, dsttype = edge.split("-")
-        
-        source = node_pairs[0]
-        destination = node_pairs[1]
-
-        graph_data[(stype, etype, dsttype)] = (torch.tensor(source), torch.tensor(destination))
-    
-    g = dgl.heterograph(graph_data)
-    g.to(device)
-    
-    for ntype in g.ntypes:
-        g.nodes[ntype].data['h'] = torch.randn(g.num_nodes(ntype), 32)
-
-    return g 
 
 def custom_collate_fn(batch):
     graphs, labels = zip(*batch)
