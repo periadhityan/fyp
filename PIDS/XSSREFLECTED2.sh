e436d0f26870b898241cce78637678f793ca057d
periadhityan <periadhityan@gmail.com>
2025-01-04 23:07:12 +0800

    Added remaining neccesary job scripts for training and testing XSSREFLECTED and XSSDOM

---
+++ PIDS/XSSREFLECTED2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=UGGPU-TC1
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=360
+#SBATCH --job-name=MalTrain
+#SBATCH --output=Job_Outputs/output_%x%j.out
+#SBATCH --error=Job_Outputs/error_%x_%j.err
+
+module load cuda/11.8
+module load anaconda
+source activate TestEnv
+python 01_train_model.py XSSREFLECTED/XSSREFLECTED_Train2 malicious XSSREFLECTED 32 Models/Benign_32.pth
