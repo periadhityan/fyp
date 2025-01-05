db629f52bcc3462fb1d06313ecbb7d978eac6c83
periadhityan <periadhityan@gmail.com>
2025-01-05 12:31:04 +0800

    Adding job name if not confusing

--- PIDS/XSSREFLECTEDTest.sh
+++ PIDS/XSSREFLECTEDTest.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=32G
 #SBATCH --ntasks-per-node=20
 #SBATCH --time=360
-#SBATCH --job-name=
+#SBATCH --job-name=ModelEval
 #SBATCH --output=Job_Outputs/output_%x%j.out
 #SBATCH --error=Job_Outputs/error_%x_%j.err
 
