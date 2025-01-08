69bfa370060914b495c37e8bd66b5b0b3bd12cb8
periadhityan <periadhityan@gmail.com>
2025-01-08 12:50:25 +0800

    Splitting train and test sets for all graphs

--- Splitting_Files/split.py
+++ Splitting_Files/split.py
@@ -3,14 +3,9 @@ import shutil
 import random
 from tqdm import tqdm
 
-source = "SQLINJECTION_Graphs"
-train1 = "SQLINJECTION_Train1"
-train2 = "SQLINJECTION_Train2"
-test = "SQLINJECTION_Test"
-
-os.makedirs(train1, exist_ok=True)
-os.makedirs(train2, exist_ok=True)
-os.makedirs(test, exist_ok=True)
+source = "BRUTEFORCE_Graphs"
+train = "BRUTEFORCE_Train"
+test = "BRUTEFORCE_Test"
 
 json_files = [f for f in os.listdir(source)]
 
@@ -19,15 +14,11 @@ random.shuffle(json_files)
 train_ratio = 0.8
 test_ratio = 0.2
 
-train1_files = json_files[:800]
-train2_files = json_files[800:1600]
+train_files = json_files[:1600]
 test_files = json_files[1600:]
 
-for file in tqdm(train1_files, desc="Creating Train Set", unit="Files"):
-    shutil.move(os.path.join(source, file), os.path.join(train1, file))
-
-for file in tqdm(train2_files, desc="Creating Train Set", unit="Files"):
-    shutil.move(os.path.join(source, file), os.path.join(train2, file))
+for file in tqdm(train_files, desc="Creating Train Set", unit="Files"):
+    shutil.move(os.path.join(source, file), os.path.join(train, file))
 
 for file in tqdm(test_files, desc="Creating Test Set", unit="Files"):
     shutil.move(os.path.join(source, file), os.path.join(test, file))
