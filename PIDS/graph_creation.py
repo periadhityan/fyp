fb3d268be489a38c4e889c31e890b93994853c23
periadhityan <periadhityan@gmail.com>
2025-01-10 10:04:30 +0800

    Adding graph to device during creation

--- PIDS/graph_creation.py
+++ PIDS/graph_creation.py
@@ -41,6 +41,7 @@ def create_graph(file, feats):
         graph_data[(stype, etype, dsttype)] = (torch.tensor(source), torch.tensor(destination))
     
     g = dgl.heterograph(graph_data)
+    g.to(device)
         
     for ntype in g.ntypes:
         g.nodes[ntype].data['h'] = torch.randn(g.num_nodes(ntype), feats)
