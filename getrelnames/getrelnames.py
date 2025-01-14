da3995705bf95be218976ed12e81b5725f50f4b1
periadhityan <periadhityan@gmail.com>
2025-01-14 15:49:34 +0800

    Minor edits

--- getrelnames/getrelnames.py
+++ getrelnames/getrelnames.py
@@ -6,6 +6,10 @@ def main():
     folder1 = "Benign_Graphs"
     folder2 = "XSSSTORED_Graphs"
     folder3 = "XSSREFLECTED_Graphs"
+    folder4 = "BRUTEFORCE_Graphs"
+    folder5 = "COMMANDINJECTION_Graphs"
+    folder6 = "SQLINJECTION_Graphs"
+    folder7 = "XSSDOM_Graphs"
 
     unique_rel_names = set()
 
@@ -22,6 +26,26 @@ def main():
     for file in tqdm(os.listdir(folder3), desc="Gathering Relation Names", unit="Graphs"):
         if file.endswith(".json"):
             json_file = os.path.join(folder3, file)
+            edges = get_etypes(json_file)
+            unique_rel_names.update(edges)
+    for file in tqdm(os.listdir(folder4), desc="Gathering Relation Names", unit="Graphs"):
+        if file.endswith(".json"):
+            json_file = os.path.join(folder4, file)
+            edges = get_etypes(json_file)
+            unique_rel_names.update(edges)
+    for file in tqdm(os.listdir(folder5), desc="Gathering Relation Names", unit="Graphs"):
+        if file.endswith(".json"):
+            json_file = os.path.join(folder5, file)
+            edges = get_etypes(json_file)
+            unique_rel_names.update(edges)
+    for file in tqdm(os.listdir(folder6), desc="Gathering Relation Names", unit="Graphs"):
+        if file.endswith(".json"):
+            json_file = os.path.join(folder6, file)
+            edges = get_etypes(json_file)
+            unique_rel_names.update(edges)
+    for file in tqdm(os.listdir(folder7), desc="Gathering Relation Names", unit="Graphs"):
+        if file.endswith(".json"):
+            json_file = os.path.join(folder7, file)
             edges = get_etypes(json_file)
             unique_rel_names.update(edges)
 
