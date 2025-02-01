7e51c5990ebdab6bc586b39c34c98cd866e6edbc
periadhityan <periadhityan@gmail.com>
2025-02-01 10:51:48 +0800

    Trying full batch again

--- PIDS/Job_Scripts/XSSREFLECTED_32.sh
+++ PIDS/Job_Scripts/XSSREFLECTED_32.sh
@@ -19,16 +19,11 @@ ATTACK=XSSREFLECTED
 FEATS=32
 EPOCHS=10
 
-# Run jobs sequentially with Load values from 1 to 16
-for i in {1..16}; do
-    if [$i eq 1]; then
-        sbatch --export=ATTACK=$ATTACK,FEATS=$FEATS,EPOCHS=$EPOCHS,LOAD_TYPE=None,SET_NUM=$i Train_Job.sh
-        sleep 1
+first_job=$(sbatch --export=ATTACK=$ATTACK,FEATS=$FEATS,EPOCHS=$EPOCHS,LOAD_TYPE=None,SET_NUM=$i Train_Job.sh | awk '{print $4}')
 
-    else
-        sbatch --export=LOAD_TYPE=Load,SET_NUM=$i Train_Job.sh
-        sleep 1
-    fi
+for i in {2..16}; do
+    next_job=$(sbatch --dependency=afterok:$first_job --export=ATTACK=$ATTACK,FEATS=$FEATS,EPOCHS=$EPOCHS,LOAD_TYPE=Load,SET_NUM=$i Train_Job.sh | awk '{print $4}')
+    first_job=$next_job
 done
 
-sbatch --export=ATTACK=$ATTACK,FEATS=$FEATS Test_Job.sh
+sbatch --dependency=afterok:$first_job --export=ATTACK=$ATTACK,FEATS=$FEATS Test_Job.sh
