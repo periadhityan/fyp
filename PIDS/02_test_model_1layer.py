767a8330c7beda4449cc6f2e381173b6bfc81388
periadhityan <periadhityan@gmail.com>
2025-02-04 09:59:20 +0800

    added test jobs for different conv layer models

---
+++ PIDS/02_test_model_1layer.py
@@ -0,0 +1,68 @@
+import dgl
+import torch
+import sys
+from torch.utils.data import DataLoader
+from sklearn.metrics import classification_report
+from graph_creation import CreatingGraphs
+from model import HeteroClassifier
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def main():
+    benign_graphs = "BENIGN/Benign_Test"
+    attack_type = sys.argv[1]
+    feats = int(sys.argv[2])
+    num_epochs = int(sys.argv[3])
+
+    results_file = f"Results/{attack_type}_{feats}_1layer_results.txt"
+    malicious_graphs = f"{attack_type}/{attack_type}_Test"
+    model_to_load = f"Models/{attack_type}_{feats}_1layer.pth"
+
+    benign_graphs, benign_labels = CreatingGraphs(benign_graphs, "Benign", feats)
+    malicious_graphs, malicious_labels = CreatingGraphs(malicious_graphs, attack_type, feats)
+
+    graphs = benign_graphs+malicious_graphs
+    labels = torch.cat([benign_labels['labels'], malicious_labels['labels']])
+
+    dataset = list(zip(graphs, labels))
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=custom_collate_fn)
+
+    file = open("rel_names.txt", "r")
+    unique_rel_names = [line.strip() for line in file.readlines()]
+    
+    model = HeteroClassifier(feats, feats, 2, unique_rel_names)
+    model.load_state_dict(torch.load(model_to_load))
+    model.to(device)
+
+    model.eval()
+    
+    predictions = []
+    labels = []
+
+    with(open(results_file, 'a')) as output:
+        output.write(f"\nEvaluating {attack_type} Model with {feats} node features and {num_epochs} epochs\n")
+
+    with torch.no_grad():
+        for graph, label in dataloader:
+            graph = graph.to(device)
+            
+            logits = model(graph)
+
+            prediction = torch.argmax(logits, dim=1)
+
+            prediction = prediction.cpu()
+            predictions.append(prediction)
+            labels.append(label)
+
+    report = classification_report(labels, predictions)
+    with(open(results_file, 'a')) as output:
+        output.write(report)
+
+def custom_collate_fn(batch):
+    graphs, labels = zip(*batch)
+    batched_graph = dgl.batch(graphs)
+    batch_labels = torch.stack(labels)
+
+    return batched_graph, batch_labels
+
+if __name__ == "__main__":
+    main()
---
+++ PIDS/Job_Scripts/XSSREFLECTED_1layer_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=UGGPU-TC1
+#SBATCH --qos=q_ug1x16
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=94G
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=960
+#SBATCH --job-name=XSSR_64_10
+#SBATCH --output=Job_Outputs/output_%x_%j.out
+#SBATCH --error=Job_Outputs/error_%x_%j.err
+#SBATCH --chdir=/home/FYP/peri0006/fyp/PIDS
+
+module load cuda/11.8
+module load anaconda
+source activate TestEnv
+
+ATTACK=XSSREFLECTED
+FEATS=64
+EPOCHS=10
+
+python 02_test_model_1layer.py $ATTACK $FEATS $EPOCHS
