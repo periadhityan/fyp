c4fe196d8e3c4ec95ec55810532a0ad5e9d8a834
periadhityan <periadhityan@gmail.com>
2025-03-12 11:25:41 +0800

    Changing QOS

--- PIDS_Complete/Job_Scripts/XSSSTORED.sh
+++ PIDS_Complete/Job_Scripts/XSSSTORED.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=UGGPU-TC1
-#SBATCH --qos=q_ug1x16
+#SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=94G
+#SBATCH --mem=64G
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=960
+#SBATCH --time=360
 #SBATCH --job-name=XSSS
 #SBATCH --output=Job_Outputs/output_%x_%j.out
 #SBATCH --error=Job_Outputs/error_%x_%j.err
 #SBATCH --chdir=/home/FYP/peri0006/fyp/PIDS_Complete
+
 
 module load cuda/11.8
 module load anaconda
--- PIDS_Complete/Job_Scripts/XSSSTORED2.sh
+++ PIDS_Complete/Job_Scripts/XSSSTORED2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=UGGPU-TC1
-#SBATCH --qos=q_ug1x16
+#SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=94G
+#SBATCH --mem=64G
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=960
+#SBATCH --time=360
 #SBATCH --job-name=XSSS
 #SBATCH --output=Job_Outputs/output_%x_%j.out
 #SBATCH --error=Job_Outputs/error_%x_%j.err
